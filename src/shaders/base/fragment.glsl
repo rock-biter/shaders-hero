13adede6dc3f64d7f6f8d958f39a1e97751820d5
rock-biter <74297920+rock-biter@users.noreply.github.com>
2025-05-03 16:14:09 +0200

    clean up code

--- src/shaders/base/fragment.glsl
+++ src/shaders/base/fragment.glsl
@@ -1,46 +1,24 @@
 #include ../functions.glsl;
 #include ../lights.glsl;
 #include ../noise.glsl;
+#include ../fbm.glsl;
+#include ../voronoi.glsl;
 
 uniform float uTime;
 uniform float uAmplitude;
 uniform float uFrequency;
-uniform int uOctaves;
-uniform sampler2D uMap;
-uniform sampler2D uNoise;
-uniform float uCurlIntensity;
-uniform int uCurlSteps;
 uniform float uParallaxSize;
 
 varying vec2 vUv;
 varying vec3 vNormal;
 varying vec3 vWorldPosition;
 varying vec3 vTangent;
-
-// #include ../perlin.glsl;
-// #include ../curl.glsl;
-// #include ../simplex.glsl;
-#include ../fbm.glsl;
-#include ../voronoi.glsl;
-
 varying vec3 vParallax;
 
 void main() {
 
-
-  // vec2 curl = curlNoise(vUv * uFrequency);
-  // float perlin = cnoise(vUv * uFrequency);
-
-  // vec3 baseColor = vec3(0.0);
-  // if(vUv.x < 0.5) {
-  //   baseColor = vec3(perlin);
-  // } else {
-  //   baseColor = vec3(curl * 0.5 + 0.5,0.0);
-  // }
-
   vec4 mapColor = vec4(0.0);
-
-  int iterations = 4;
+  int iterations = 2;
 
   for(int i = 0; i < iterations; i++) {
     vec2 uv = vec2(vUv - vParallax.xy * (float(i) * 1. * uParallaxSize + 0.3));
@@ -57,23 +35,10 @@ void main() {
     mapColor.rgb += vec3(0.,c,c * float(iterations + 1 - i) / float(iterations + 1));
   }
 
-  // mapColor.rgb /= 3.;
-  // mapColor.a /= 2.;
-
-  // vec2 uv = vec2(vUv - vParallax.xy);
-
-  // vec2 t = 1.0 - step(1.01,uv);
-  // t *= step(-0.01,uv);
-
-  // vec3 mapColor = texture2D(uMap, uv).rgb;
-
 
 	vec3 color = vec3(mapColor.rgb);
-	// vec3 color = vec3(0.);
-  // color = mix(color, vec3(1.0), 1.0 - min(t.x,t.y));
-  // color.r = vTangent.r * 0.5 + 0.5;
-  // color.g = vTangent.g * 0.5 + 0.5;
-  // color.b = vTangent.b * 0.5 + 0.5;
-
   gl_FragColor = vec4(color,mapColor.a);
+
+  #include <tonemapping_fragment>
+  #include <colorspace_fragment>
 }
--- src/shaders/base/vertex.glsl
+++ src/shaders/base/vertex.glsl
@@ -7,7 +7,6 @@ varying vec3 vWorldPosition;
 uniform float uTime;
 uniform float uAmplitude;
 uniform float uFrequency;
-uniform sampler2D uNoise;
 uniform float uParallaxSize;
 
 
@@ -15,42 +14,6 @@ uniform float uParallaxSize;
 #include ../perlin.glsl;
 
 varying vec3 vParallax;
-
-vec3 projectOnPlane(vec3 v, vec3 n) {
-    // Formula: v - (v·n)n
-    // dove · è il prodotto scalare
-    return v - dot(v, n) * n;
-}
-
-mat3 alignVector(vec3 v, vec3 target) {
-    // Normalizza i vettori
-    vec3 v_n = normalize(v);
-    vec3 target_n = normalize(target);
-    // v_n *= vec3(-1.0,-1.0 ,-1.0);
-    
-    // Calcola l'asse di rotazione tramite prodotto vettoriale
-    vec3 axis = -cross(v_n, target_n);
-    
-    // Calcola l'angolo tra i vettori usando il prodotto scalare
-    float cosAngle = dot(v_n, target_n);
-    float angle = acos(cosAngle);
-    
-    // Costruisci la matrice di rotazione (formula di Rodrigues)
-    float s = sin(angle);
-    float c = cosAngle;
-    float t = 1.0 - c;
-    
-    // Costruisci la matrice di rotazione
-    mat3 rotMatrix = mat3(
-        t * axis.x * axis.x + c,        t * axis.x * axis.y - s * axis.z, t * axis.x * axis.z + s * axis.y,
-        t * axis.x * axis.y + s * axis.z, t * axis.y * axis.y + c,        t * axis.y * axis.z - s * axis.x,
-        t * axis.x * axis.z - s * axis.y, t * axis.y * axis.z + s * axis.x, t * axis.z * axis.z + c
-    );
-    
-    // Applica la rotazione al vettore originale
-    return rotMatrix;
-}
-
 varying vec3 vTangent;
 
 void main() {
@@ -64,12 +27,10 @@ void main() {
   vTangent = biTangent;
 
   vec3 pos = position;
-  // pos.z += cnoise(position.xy * uFrequency) * uAmplitude;
   vec4 wPos = (modelMatrix * vec4(pos,1.0));
 
   vec3 camDir = normalize(tbn * cameraPosition - tbn * wPos.xyz);
   camDir = camDir;
-  // vParallax = projectOnPlane(camDir, vec3(0.0,0.0,1.0));
   vParallax.xy = camDir.xy;
 
   // scala il vettore di parallax
